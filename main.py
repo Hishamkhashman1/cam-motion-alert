5ecd17e3a208d73f81234b5268ac0fd4951ce8b3
Hisham <hisham.khashman@gmail.com>
2026-04-21 17:43:07 +0900

    structure

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import time
+from datetime import datetime
+from pathlib import Path
+
+import cv2
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+
+# Define variables (what URL for RTSP with username and password in .env, TWILIO account for whatsapp notifications , account, toke, whatsapp from , whatsapp to, and SNAPshot URL for sending the snapshot of movement with notification.
+
+# Define control area, alert cooldown and frame wedth
+
+
+# raise errors if RTSP link not present 
+
+# raise errors if TWILIO is not funcitonal
+
+# define send whatsapp alert funciton
+
+# define save snapshot funciton
+
+# define build snapshot function
+
+# define preprocess
+
+# define main funciton 
+
