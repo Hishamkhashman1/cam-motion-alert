b134eda063cdb636a296703aced01e262044b05e
Hisham <hisham.khashman@gmail.com>
2026-04-21 18:41:40 +0900

    added main py func

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from detector import run_motion_detector
 
 # TODO: Import and invoke `run_motion_detector` from `detector.py` as the app entrypoint.
+
+if __name__ == "__main__":
+    run_motion_detector()
